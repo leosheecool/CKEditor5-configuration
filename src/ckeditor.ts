8e451a0ac437539ada6befdfb93c32148742bc87
Léo Levacher <leo.levacher@actency.fr>
2024-09-18 15:46:16 +0200

    refactor: Update CKEditor dependencies and remove unused files

--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -3,52 +3,44 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { InlineEditor } from '@ckeditor/ckeditor5-editor-inline';
-
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import {
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { InlineEditor,
+Alignment,
+Autoformat,
   Bold,
   Italic,
   Superscript,
-  Underline
-} from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import type { EditorConfig } from '@ckeditor/ckeditor5-core';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { FontColor, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import {
+  Underline,
+BlockQuote,
+Essentials ,
+FontColor, FontSize ,
+Heading,
   Image,
   ImageCaption,
   ImageInsert,
   ImageResize,
   ImageStyle,
   ImageToolbar,
-  ImageUpload
-} from '@ckeditor/ckeditor5-image';
-import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import {
+  ImageUpload,
+Link, LinkImage ,
+List ,
+MediaEmbed, MediaEmbedToolbar,
+Paragraph,
+PasteFromOffice,
   Table,
   TableCaption,
   TableCellProperties,
   TableColumnResize,
   TableProperties,
-  TableToolbar
-} from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
-import { Undo } from '@ckeditor/ckeditor5-undo';
-import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
+  TableToolbar,
+TextTransformation ,
+Undo,
+FindAndReplace} from 'ckeditor5';
 import ImageStyleUtils from '@ckeditor/ckeditor5-image/src/imagestyle/utils';
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
 
-// @ts-expect-error
 class Editor extends InlineEditor {
   public static override readonly builtinPlugins = [
     FindAndReplace,
